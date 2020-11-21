2bcc188c9ee6b2fe35a55e00b69a541cc5490ce1
Ashwin Padaki <apadaki22@gmail.com>
2020-11-21 10:07:31 -0500

    updated guide to include all latent variables

--- mixed-membership/mixedmem.py
+++ mixed-membership/mixedmem.py
@@ -4,10 +4,11 @@ import pyro
 import os
 from pyro.optim import Adam
 from pyro.infer import SVI, Trace_ELBO
+import matplotlib.pyplot as plt
 
 def data_generation(K, N, J, n):
-    var_param_1 = 1
-    var_param_2 = 2
+    var_param_1 = 2
+    var_param_2 = 3
     var_param_3 = [[None] * K for _ in range(J)]
     for j in range(J):
         for k in range(K):
@@ -41,7 +42,7 @@ def data_generation(K, N, J, n):
 CLASSES = 2
 INDIVIDUALS = 5
 QUESTIONS = 4
-NUM_CHOICES = [3, 2, 1, 4]
+NUM_CHOICES = [3, 2, 3, 4] # jth element = number of answer choices for question j
 g, z, y, lam = data_generation(CLASSES, INDIVIDUALS, QUESTIONS, NUM_CHOICES)
 data = y # data is an NxJ matrix corresponding to an answer for a given question for a given individual
 
@@ -49,15 +50,15 @@ print(data)
     
 
 def model(K, N, J, n, data):
-    var_param_1 = 1
-    var_param_2 = 2
+    var_param_1 = 2
+    var_param_2 = 3
     var_param_3 = [[None] * K for _ in range(J)]
     for j in range(J):
         for k in range(K):
             var_param_3[j][k] = torch.ones(n[j])
 
     alpha0 = pyro.sample('alpha0', pyro.distributions.Gamma(var_param_1, var_param_2))
-    xi = pyro.sample('xi', pyro.distributions.Dirichlet(torch.ones(K)));
+    xi = pyro.sample('xi', pyro.distributions.Dirichlet(torch.ones(K)))
 
     alpha = alpha0 * xi
 
@@ -82,38 +83,66 @@ def model(K, N, J, n, data):
     return y
 
 def guide(K, N, J, n, data):
-    var_param_1 = pyro.param("var_param_1", torch.tensor(3), constraint = constraints.positive) # Finalize initial guesses later 
-    var_param_2 = pyro.param("var_param_2", torch.tensor(4), constraint = constraints.positive) 
+    var_param_1 = pyro.param("var_param_1", torch.tensor(2.5), constraint = constraints.positive) # Finalize initial guesses later 
+    var_param_2 = pyro.param("var_param_2", torch.tensor(3.5), constraint = constraints.positive) 
     var_param_3 = [[None] * K for _ in range(J)]
+    var_param_4 = pyro.param("var_param_4", torch.ones(K), constraint = constraints.positive)
     for j in range(J):
         for k in range(K):
             var_param_3[j][k] = pyro.param("var_param_3_j{}k{}".format(j, k), torch.ones(n[j]), constraints.positive)
     
-    pyro.sample("alpha0", pyro.distributions.Gamma(var_param_1, var_param_2))
+    alpha0 = pyro.sample("alpha0", pyro.distributions.Gamma(var_param_1, var_param_2))
+    xi = pyro.sample("xi", pyro.distributions.Dirichlet(var_param_4))
 
     for j in range(J):
         for k in range(K):
             pyro.sample("lambda_data_j{}k{}".format(j, k), pyro.distributions.Dirichlet(var_param_3[j][k]))
+
+    for i in pyro.plate("membership_loop", N):
+        # sample g_i from dirichlet distribution
+        g[i] = pyro.sample("gdata_{}".format(i), pyro.distributions.Dirichlet(alpha0*xi))
+
+        for j in pyro.plate("question_loop_{}".format(i), J):
+            # sample z_ij from discrete distribution
+            z[i][j] = pyro.sample("zdata_{}{}".format(i, j), pyro.distributions.Categorical(g[i]))
                 
 # print("\n"+str(model(2, 5, 4, [3, 2, 1, 4], data)))
 
 # set up the optimizer
-adam_params = {"lr": 0.0005, "betas": (0.90, 0.999)}
+adam_params = {"lr": 0.01, "betas": (0.90, 0.999)}
 optimizer = Adam(adam_params)
 
 # setup the inference algorithm
 svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
 
-n_steps = 5000
+n_steps = 2500
+
+ELBO_CHANGES = []
+
 # do gradient steps
 for step in range(n_steps):
-    svi.step(CLASSES, INDIVIDUALS, QUESTIONS, NUM_CHOICES, data)
+    ELBO = str(svi.step(CLASSES, INDIVIDUALS, QUESTIONS, NUM_CHOICES, data))
     if step % 10 == 0:
-        print('n=', step)
+        print('n =', step)
+        ELBO_CHANGES.append(ELBO)
+        # print(ELBO)
+        print(pyro.param('var_param_1').item())
+        print(pyro.param('var_param_2').item())
+
+
+print("XI: " + str(pyro.param('var_param_4')))
+
+# plt.plot(ELBO_CHANGES)
+# plt.title("ELBO")
+# plt.ylabel("step")
+# plt.xlabel("n");
+
+
 
 var_param_1 = pyro.param("var_param_1").item()
 var_param_2 = pyro.param("var_param_2").item()
 var_param_3 = [[None] * CLASSES for _ in range(QUESTIONS)]
+var_param_4 = pyro.param("var_param_4")
 
 for j in range(QUESTIONS):
     for k in range(CLASSES):
